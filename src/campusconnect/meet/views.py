9198d911aaf1ca76647915e6a56fa55fc0edd189
MukundiCode <tinashechitamba@outlook.com>
2021-05-07 23:20:17 +0200

    Splitting interests into list

--- src/campusconnect/meet/views.py
+++ src/campusconnect/meet/views.py
@@ -6,21 +6,28 @@ from django.db.models import Q
 from django.http import HttpResponseRedirect
 import json
 from .forms import profileForm
+from .models import *
 
 def index(request):
     return render(request,'index.html')
 
 
 def createProfile(request):
+    profileObj = Profile.objects.all()
+    for p in profileObj:
+            interestsList = p.interests.split()
+            print(interestsList)
     if request.method == "POST":
         form = profileForm(request.POST)
-        print(form)
         if form.is_valid():
             print('valid')
             form.save()
-            return redirect('index')
+            return redirect('createProfile')
         else:
             print('not valid')
     form = profileForm
     context = {'form':form}
     return render(request,'createProfile.html',context)
+
+#def matcher(user):
+
