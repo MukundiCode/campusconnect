29e7f1448bb84a3b8f7d683b1200f24db988767e
MukundiCode <tinashechitamba@outlook.com>
2021-05-07 21:26:29 +0200

    Adding static files

--- src/campusconnect/campusconnect/settings.py
+++ src/campusconnect/campusconnect/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'meet/static')]
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
