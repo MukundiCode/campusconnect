804b0b518866d7de4ec2b7a9f79edbd192454fcf
MukundiCode <tinashechitamba@outlook.com>
2021-05-08 04:03:03 +0200

    Fixed error in forms.py

--- src/campusconnect/meet/forms.py
+++ src/campusconnect/meet/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 #from accounts.models import Account
-from meet.models import Profile
+from meet.models import Profile,Event,MeetUp
 
 #Tinashe Mukundi Chitamba
 #This script deals with forms. It takes the data inputed in a form and populates
@@ -15,10 +15,10 @@ class profileForm(forms.ModelForm):
         
 class eventForm(forms.ModelForm):
     class Meta:
-        model = Profile
+        model = Event
         fields = ['date','poster','limit']
 
 class meetupForm(forms.ModelForm):
     class Meta:
-        model = Profile
+        model = MeetUp
         fields = ['location','time','description']
